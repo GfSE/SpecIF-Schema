4da249405a8090f0042c3324a1d42fa5e21a3173
odungern <od@enso-managers.de>
2023-07-01 10:51:38 +0200

    new regex for isoDateTime

--- check/CCheck.mjs
+++ check/CCheck.mjs
@@ -8,8 +8,8 @@
 export class CCheck {
     constructor() {
         this.ajv = new Ajv({allErrors: true});
-		this.RE = {
-			IsoDateTime: /^(\d{4}(?!\d{2}\b))((-?)((0[1-9]|1[0-2])(\3([12]\d|0[1-9]|3[01]))?|W([0-4]\d|5[0-2])(-?[1-7])?|(00[1-9]|0[1-9]\d|[12]\d{2}|3([0-5]\d|6[1-6])))([T\s]((([01]\d|2[0-3])((:?)[0-5]\d)?|24\:?00)([\.,]\d+(?!:))?)?(\17[0-5]\d([\.,]\d+)?)?([zZ]|([\+-])([01]\d|2[0-3]):?([0-5]\d)?)?)?)?$/
+		this.regex = {
+			IsoDateTime: /^(\d{4})-(0[1-9]|1[0-2])-(0[1-9]|[1-2]\d|30|31)(?:T([0-1]\d|2[0-4]):([0-5]\d):([0-5]\d(?:\.\d{1,3})?)(\+(0\d|11|12):([0-5]\d)|-(0\d|11|12):([0-5]\d)|Z)?)?$/
 		};
     }
     checkSchema( data, options ) {
@@ -393,7 +393,7 @@ export class CCheck {
                                     errorL.push({status:986, statusText:etxt+": boolean value is invalid"}); 
                                 break;
                             case 'xs:dateTime':
-								if( !self.RE.IsoDateTime.test(val) )
+								if( !self.regex.IsoDateTime.test(val) )
                                     errorL.push({status:986, statusText: etxt+": value must be a valid date-time string according to ISO 8601"});
                                 break;
                             case 'xs:duration':
